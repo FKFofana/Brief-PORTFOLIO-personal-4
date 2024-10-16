f1f91dca002a34606e0ac5426069e0529fd8f8f0
FKFofana <fofanafatoukine1@gmail.com>
2024-10-16 22:56:14 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+    const mobileMenu = document.getElementById('mobile-menu');
+    const navList = document.querySelector('.nav-list');
+
+    mobileMenu.addEventListener('click', () => {
+        navList.classList.toggle('show');
+    });
+</script>
